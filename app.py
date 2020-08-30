1d37e543207c3109fbb2b32a0a48239242069f20
deekshashakun <deekshashakun@gmail.com>
2020-08-31 00:20:45 +0530

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+
 
 app=Flask(__name__)
 
 @app.route('/')
-def index():
-   return render_template('index.html')
+def student():
+   return render_template('home.html')
+
+@app.route('/index')
+def select():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
+
+
+
 
 
 
